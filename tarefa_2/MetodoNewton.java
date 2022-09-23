0c6a2c86ff7c487eda684e11609273d22a09d27d
Fernando Sousa <96151093+feropiso@users.noreply.github.com>
2022-09-23 00:08:29 -0300

    Update MetodoNewton.java

--- tarefa_2/MetodoNewton.java
+++ tarefa_2/MetodoNewton.java
@@ -1,50 +1,55 @@
-class MetodoNewton {
-  public static void main(String[] args) {
-    encontraZeros(new Funcao() {
-      @Override
-      public double f(double x) {
-        return Math.pow(x, 3)-27;
-      }
-    });
-  }
 
-  public static interface Funcao{
-    public double f(double x);
-  }
 
-  public static void encontraZeros(Funcao f){
-    
-    int contador = 0;
-    //Aproximação
-    double x0 = 5.0;
-    
-    double x = 0.0;
-    double x1 = 0.0;
-    double erro = 0.000;
-    
-    while(true){
+public class MetodoNewton {
+	
+	 public static void main(String[] args) {
+		    encontraZeros(new Funcao() {
+		      @Override
+		      public double f(double x) {
+		        return Math.pow(x, 6) + 3*Math.pow(x, 5) + 4*Math.pow(x, 3) - Math.pow(7, x);
+		      }
+		    });
+		  }
 
-      if (Math.abs(f.f(x0)) <= erro){
-        x = x0;
-        System.out.println("O zero da função é: "+x);
-        System.out.println("O número de iterações: "+contador);
-        break;
-      }
-      contador++;
+		  public static interface Funcao{
+		    public double f(double x);
+		  }
 
-      x1 = x0 - f.f(x0)/(3*Math.pow(x0, 2));
+		  public static void encontraZeros(Funcao f){
+		    
+		    int contador = 0;
+		    //Aproximação
+		    double x0 = -1.0;
+		    
+		    double x = 0.0;
+		    double x1 = 0.0;
+		    double erro = 0.0001;
+		    
+		    while(true){
 
-      if (Math.abs(f.f(x1)) <= erro || Math.abs(x1-x0) <= erro){
-        x = x1;
-        System.out.println("O zero da função é: "+x);
-        System.out.println("O número de iterações: "+contador);
-        break;
-      }
+		      if (Math.abs(f.f(x0)) <= erro){
+		        x = x0;
+		        System.out.println("O zero da função é: "+x);
+		        System.out.println("O número de iterações: "+contador);
+		        break;
+		      }
+		      contador++;
 
-      x0 = x1;
-      
-      contador++;
-      
-    }    
-  }
+		      x1 = x0 - f.f(x0)/(6*Math.pow(x0, 5) + 15*Math.pow(x0, 4) + 12*Math.pow(x0, 2) - Math.log10(7)*Math.pow(7, x0));
+
+		      if (Math.abs(f.f(x1)) <= erro || Math.abs(x1-x0) <= erro){
+		        x = x1;
+		        System.out.println("O zero da função é: "+x);
+		        System.out.println("O número de iterações: "+contador);
+		        break;
+		      }
+
+		      x0 = x1;
+		      
+		      contador++;
+		      
+		    }    
+		  }
+
 }
+
